e7130215c36f43d1cd3941e104cbebd032002108
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-12 00:22:11 +0530

    update for dynamic values

--- excelDemo.js
+++ excelDemo.js
@@ -1,21 +1,23 @@
 const ExcelJs = require("exceljs");
 
 async function excelTest() {
+
+  let output = {row:-1, column:-1};
   const workbook = new ExcelJs.Workbook();
   await workbook.xlsx.readFile("downloads/exceldownloadTest.xlsx");
   const worksheet = workbook.getWorksheet("Sheet1");
 
   worksheet.eachRow((row, rowNumber) => {
     row.eachCell((cell, colNumber) => {
-      if(cell.value === 'Apple'){
-        console.log(rowNumber);
-        console.log(colNumber);
+      if (cell.value === "Banana") {
+       output.row = rowNumber;
+       output.column = colNumber;
       }
     });
   });
-  
-  const cell = worksheet.getCell(4,2);
-  cell.value = 'Iphone';
+
+  const cell = worksheet.getCell(output.row, output.column);
+  cell.value = "Republic";
   await workbook.xlsx.writeFile("downloads/exceldownloadTest.xlsx");
 }
 
