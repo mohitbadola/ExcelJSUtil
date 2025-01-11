5b58ba77241106aa81bb8704a99d7a702e4ab821
Mohit Badola <94218660+mohitbadola@users.noreply.github.com>
2025-01-12 00:42:33 +0530

    optimize texcelDemo.js

--- excelDemo.js
+++ excelDemo.js
@@ -1,24 +1,28 @@
 const ExcelJs = require("exceljs");
 
-async function excelTest() {
-
-  let output = {row:-1, column:-1};
-  const workbook = new ExcelJs.Workbook();
-  await workbook.xlsx.readFile("downloads/exceldownloadTest.xlsx");
-  const worksheet = workbook.getWorksheet("Sheet1");
-
-  worksheet.eachRow((row, rowNumber) => {
-    row.eachCell((cell, colNumber) => {
-      if (cell.value === "Banana") {
-       output.row = rowNumber;
-       output.column = colNumber;
-      }
+async function readExcel(worksheet, searchText) {
+    let output = { row: -1, column: -1 };
+    worksheet.eachRow((row, rowNumber) => {
+      row.eachCell((cell, colNumber) => {
+        if (cell.value === searchText) {
+          output.row = rowNumber;
+          output.column = colNumber;
+        }
+      });
     });
-  });
+    return output;
+  }
+
+async function writeExcelTest(searchText, replaceText, filePath) {
+  const workbook = new ExcelJs.Workbook();
+  await workbook.xlsx.readFile(filePath);
+  const worksheet = workbook.getWorksheet("Sheet1");
+  const output = await readExcel(worksheet, searchText);
 
   const cell = worksheet.getCell(output.row, output.column);
-  cell.value = "Republic";
-  await workbook.xlsx.writeFile("downloads/exceldownloadTest.xlsx");
+  cell.value = replaceText;
+  await workbook.xlsx.writeFile(filePath);
 }
 
-excelTest();
+
+writeExcelTest("Republic", "Banana", "downloads/exceldownloadTest.xlsx");
